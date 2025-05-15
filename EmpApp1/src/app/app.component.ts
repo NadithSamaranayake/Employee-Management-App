c9189052bd99294f99300e0f9e1d4614f2b3b735
Nadith Samaranayake <nadith.jsamaranayake@gmail.com>
2025-05-15 16:16:48 +0530

    Created nav bar and emp list components

--- EmpApp1/src/app/app.component.ts
+++ EmpApp1/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeListComponent } from "./employee-list/employee-list.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'emp-root',
+  imports: [RouterOutlet, EmployeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ EmpApp1/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'employee-list',
+  imports: [CommonModule],
+  templateUrl: './employee-list.component.html',
+  styleUrl: './employee-list.component.scss'
+})
+export class EmployeeListComponent {
+
+}
